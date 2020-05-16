3b5d5b8aae9a610b21b7ad8d45fb0018c402dfd6
DosU <Doslan130400@gmail.com>
2020-05-16 22:34:09 +0600

    commit

--- src/main/java/dosu/com/final_project/FinalProjectApplication.java
+++ src/main/java/dosu/com/final_project/FinalProjectApplication.java
@@ -1,13 +1,25 @@
 package dosu.com.final_project;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class FinalProjectApplication {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(FinalProjectApplication.class, args);
     }
 
+
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
 }
