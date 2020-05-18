d325af1ec6e386fa7bc4eca6ba1139225e27aec6
DosU <Doslan130400@gmail.com>
2020-05-18 15:46:46 +0600

    commit

--- src/main/java/dosu/com/final_project/Screen.java
+++ src/main/java/dosu/com/final_project/Screen.java
@@ -1,4 +1,60 @@
 package dosu.com.final_project;
 
+import javax.persistence.*;
+
+@Entity
 public class Screen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name="name")
+    private String name;
+    @Column(name="url")
+    private String url;
+    @Column(name="width")
+    private int width;
+    @Column(name="height")
+    private int height;
+
+    public Screen(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
